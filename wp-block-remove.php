3aaf4173257220551a0438634e07d433f04670a2
Ilmars Millers <lapa@lapulapa.eu>
2022-04-11 10:37:59 +0300

    Update wp-block-remove.php

--- wp-block-remove.php
+++ wp-block-remove.php
@@ -7,3 +7,7 @@ function wp_remove_wp_block_library_css(){
     wp_dequeue_style( 'global-styles' ); 
 } 
 add_action( 'wp_enqueue_scripts', 'wp_remove_wp_block_library_css', 100 );
+
+add_filter('use_block_editor_for_post', '__return_false', 10);
+add_filter( 'gutenberg_use_widgets_block_editor', '__return_false', 100 );
+add_filter( 'use_widgets_block_editor', '__return_false' );
